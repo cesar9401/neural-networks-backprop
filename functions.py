147a4cb42394a84c9b76275c3a9c67d5e8e95ea6
César Tzoc <csartalva01@gmail.com>
2024-05-16 03:08:11 -0600

    feat: adding functions

---
+++ functions.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def sigmoid_prime(z):
+    return sigmoid(z) * (1 - sigmoid(z))
+
+
+def hyperbolic_tangent(z):
+    return (np.exp(2 * z) - 1) / (np.exp(2 * z) + 1)
+
+
+def hyperbolic_tangent_prime(z):
+    return pow((2 * np.exp(z)) / (np.exp(2 * z) + 1), 2)
+
+
+def identity_function(z):
+    return z
+
+
+def identity_function_prime(z):
+    return 1
+
+
+def step_function(z):
+    return 1 if z >= 0 else 0
+
+
+def step_function_prime(z):
+    return 0
