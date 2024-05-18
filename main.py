e9533ff6583bd1d9315b365820a945d5d918b7cc
César Tzoc <csartalva01@gmail.com>
2024-05-18 14:59:20 -0600

    feat: adding ui

--- main.py
+++ main.py
@@ -1,6 +1,71 @@
+from PyQt5.QtWidgets import QApplication, QStyleFactory
 import sys
+import re
 
+from network import Network
 from ui import main_widget
+
+import pandas as pd
+import numpy as np
+
+REGEX_INPUT = 'i[1-9][0-9]*'
+REGEX_OUTPUT = 'o[1-9][0-9]*'
+
+
+class Main:
+    def __init__(self):
+        self.network: Network | None = None
+
+        app = QApplication(sys.argv)
+        app.aboutToQuit.connect(app.deleteLater)
+        app.setStyle(QStyleFactory.create('gtk'))
+        self.window = main_widget.MainWidget(self)
+        self.window.show()
+        sys.exit(app.exec_())
+
+    def start_training(self, layer_sizes, training_file, hidden_function, output_function, epochs, learning_rate):
+        training_data = self.read_file(training_file)
+
+        self.network = Network(layer_sizes, hidden_function, output_function)
+        self.network.SGD(training_data, epochs, 1, learning_rate, None)
+
+    def start_testing(self, testing_file):
+        if self.network is None:
+            return
+
+        testing_data = self.read_file(testing_file)
+        for x, expected in testing_data:
+            output = self.network.feedforward(x)
+            print(f'(result {expected == output})\t\texpected: {expected} -> output: {output}')
+
+    def read_file(self, filename):
+        df, in_cols, out_cols = self.get_total_inputs_and_outputs(filename)
+
+        total_inputs, total_outputs = [], []
+        for index, row in df.iterrows():
+            inputs = []
+            for col in in_cols:
+                inputs.append(row[col])
+            total_inputs.append(inputs)
+
+            outputs = []
+            for col in out_cols:
+                outputs.append(row[col])
+            total_outputs.append(outputs)
+
+        file_input = np.array(total_inputs)
+        file_output = np.array(total_outputs)
+        return [(x.reshape(-1, 1), y.reshape(-1, 1)) for x, y in zip(file_input, file_output)]
+
+    @staticmethod
+    def get_total_inputs_and_outputs(filename):
+        df = pd.read_csv(filename)
+
+        in_cols = [col for col in df.columns if re.search(REGEX_INPUT, col)]
+        out_cols = [col for col in df.columns if re.search(REGEX_OUTPUT, col)]
+
+        return df, in_cols, out_cols
+
 
 # from network import Network
 # import numpy as np
@@ -40,12 +105,6 @@ from ui import main_widget
 #
 # # result = network1.evaluate(test_data)
 # # print(result)
-from PyQt5.QtWidgets import QApplication, QStyleFactory
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    app.aboutToQuit.connect(app.deleteLater)
-    app.setStyle(QStyleFactory.create('gtk'))
-    window = main_widget.MainWidget()
-    window.show()
-    sys.exit(app.exec_())
+    main = Main()
